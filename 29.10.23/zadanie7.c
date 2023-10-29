d1e86dfc51b3aa6b69f8e8b7f88cdbfdd020c270
Mateusz Gruba <mgruba2@linux-A005-20.inf.ug.edu.pl>
2023-10-29 15:10:31 +0100

    pierwsza fala zadań

--- 29.10.23/zadanie7.c
+++ 29.10.23/zadanie7.c
@@ -1,1 +1,25 @@
-zadanie7.c
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int dzien;
+    int miesiac;
+    int rok;
+    printf("Podaj dzien z przedzialu 1-31: \n");
+    scanf("%d",&dzien);
+    printf("Podaj miesiac z przedzialu 1-12: \n");
+    scanf("%d",&miesiac);
+    printf("Podaj rok: \n");
+    scanf("%d",&rok);
+     if ((dzien>=1||dzien<=31)&&(miesiac>=1||miesiac<=12))
+     {
+        printf("Data jest prawidłowa %d-%d-%d",dzien,miesiac,rok);
+     }
+     else
+     {
+        printf("data jest nieprawidłowa");
+     }
+    return 0;
+    
+}
