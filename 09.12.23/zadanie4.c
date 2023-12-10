a95c08d523d1c2d2bd9999661a9945e71b1f5bf8
Mateusz Gruba <mgruba555@gmail.com>
2023-12-10 14:31:08 +0100

    09.12.23

--- 09.12.23/zadanie4.c
+++ 09.12.23/zadanie4.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
-int NET (int rozmiar = 10, const int tab[*rozmiar])
+int net(int *wskaznik, int n)
 {
-    
-
+    int max=0;
+    for(int i=0;i<n;i++)
+    {
+        if(wskaznik[i]>max)
+        {
+            max=wskaznik[i];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(wskaznik[i]==max)
+        {
+        wskaznik[i]=0;
+        }
+    }
+    return max;
+}
+int main()
+{
+    int n=10;
+    int tab[10];
+    for(int i=0;i<10;i++)
+    {
+        tab[i]=i;
+    }
+    int *wskaznik=tab;
+    printf("%d",net(wskaznik,n));
+    printf("\n");
+    return 0;
 }
---
+++ 09.12.23/zadanie5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int sort(int a, int b, int c)
+{
+    int *liczba1 = &a;
+    int *liczba2 = &b;
+    int *liczba3 = &c;
+   
+}
